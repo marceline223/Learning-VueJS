f18ed3ddcd7be8387cf15643e3fd515ce19eeff8
Dasha <dasha220301@gmail.com>
2023-03-01 15:34:12 +0300

    Add forms for HW 1

---
+++ lesson-1/script.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#form',
+    data: {
+        email: '',
+        firstName: '',
+        lastName: '',
+        phone: '',
+        countGuests: 0,
+        guests: []
+    },
+    methods: {
+        addGuest(guest){
+            if (guest) {
+                this.guests.push(guest)
+            }
+        }
+    }
+})
