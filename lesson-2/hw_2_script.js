db05991755931407098bb5c4a9504855c20965a4
Dasha <dasha220301@gmail.com>
2023-03-10 01:06:02 +0300

    Done the task 2

--- lesson-2/hw_2_script.js
+++ lesson-2/hw_2_script.js
@@ -1,6 +1,18 @@
 new Vue({
     el: '.sample',
     data: {
-
+        isAgreed: false,
+        isSpam: false,
+        sendSpamOn: 'email',
+        isRead: false,
+        showResult: false,
+        hasScrolledToBottom: false
+    },
+    methods: {
+        handleScroll(el) {
+            if ((el.target.offsetHeight + el.target.scrollTop) >= el.target.scrollHeight) {
+                this.isRead = true;
+            }
+        }
     }
 });
